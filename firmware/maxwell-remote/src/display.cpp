66c3332564f1541a6a2960fd655151e4f0cf7276
Adam Coddington <me@adamcoddington.net>
2018-03-26 19:09:06 -0700

    Adding misc Michroma font sizes.

--- firmware/maxwell-remote/src/display.cpp
+++ firmware/maxwell-remote/src/display.cpp
@@ -186,7 +186,7 @@ void DisplayManager::refresh() {
                 display.println("<FLASH>");
                 break;
             default:
-                display.println("");
+                display.println("<STARTING>");
                 break;
         }
     }
