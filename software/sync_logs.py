12325147a8434ec0a113bd0cd011cf7ddf9707f2
Adam Coddington <me@adamcoddington.net>
2019-11-02 19:01:46 -0700

    Updating sync_logs script to use crc32/checksum-equipped log download functions.

--- software/sync_logs.py
+++ software/sync_logs.py
@@ -1,8 +1,10 @@
 import argparse
+import binascii
+import contextlib
+import datetime
 import logging
 import os
 import sys
-import time
 
 import serial
 from typing import Iterable, List
@@ -11,21 +13,26 @@ from typing import Iterable, List
 logger = logging.getLogger(__name__)
 
 
-def send_command(connection, cmd, *args) -> Iterable[str]:
+def send_command(connection, cmd, *args):
     full_command = ' '.join([
         cmd,
-        *args
+        *(str(arg) for arg in args)
     ])
     connection.write(full_command.encode('utf-8'))
-    connection.write('\n'.encode('utf-8'))
+    connection.write(b'\n')
 
-    is_first_line = True
+    logger.debug("Sending command: %s", full_command)
+
+    # Consume the echo'd back command
+    connection.readline()
+
+
+def get_response_for_command(connection, cmd, *args) -> Iterable[str]:
+    send_command(connection, cmd, *args)
+
     while True:
         line = connection.readline()
-        if is_first_line:
-            is_first_line = False
-            continue
-        if(not line):
+        if(not line.strip()):
             break
 
         if line.decode('utf-8').strip().startswith('>>'):
@@ -34,11 +41,49 @@ def send_command(connection, cmd, *args) -> Iterable[str]:
         yield line.decode('utf-8').rstrip()
 
 
+@contextlib.contextmanager
+def timeout(connection, timeout):
+    original_timeout = connection.timeout
+    connection.timeout = timeout
+
+    yield
+
+    connection.timeout = original_timeout
+
+
+def get_verified_response_for_command(
+    connection,
+    cmd,
+    *args,
+    max_attempts=10
+) -> Iterable[str]:
+    last = []
+
+    attempts = 0
+
+    while attempts < max_attempts:
+        current = list(get_response_for_command(connection, cmd, *args))
+
+        if current == last:
+            for line in current:
+                yield line
+
+            return
+
+        last = current
+        attempts += 1
+
+    raise ValueError("Failed to get verified response")
+
+
 def list_logs(connection) -> List[str]:
     indentations = {}
     logs = []
 
-    for line in send_command(connection, 'log_list'):
+    for line in get_response_for_command(
+        connection,
+        'log_list',
+    ):
         indentation_level = len(line) - len(line.lstrip())
         indentations[indentation_level] = line.strip()
 
@@ -57,12 +102,58 @@ def list_logs(connection) -> List[str]:
 
 
 def get_log(connection, path) -> Iterable[str]:
-    for line in send_command(connection, 'log_print', path):
-        yield line
+    send_command(
+        connection,
+        'log_print_chk',
+        path
+    )
+    line_count = 0
+    error_count = 0
+    while True:
+        line = connection.readline()
+        line_count += 1
+        if not line.strip():
+            break
+
+        if line.decode('utf-8').strip().startswith('>>'):
+            return
+
+        try:
+            data, checksum = line.split(b'\t::::\t')
+        except ValueError:
+            # We probably lost a character in our delimiter
+            connection.write(b'0')
+            error_count += 1
+            continue
+
+        if binascii.crc32(data) == int(checksum):
+            connection.write(b'1')
+        else:
+            connection.write(b'0')
+            error_count += 1
+
+        yield data.decode('utf-8').strip()
+
+    logger.info(
+        'Successfully received (error rate %s%%)',
+        error_count / line_count * 100
+    )
+
+
+def log_checksum(connection, filename) -> int:
+    with timeout(connection, 10):
+        lines = ' '.join(get_verified_response_for_command(
+            connection,
+            'log_crc32',
+            filename,
+        ))
+        if 'Error' in lines:
+            return 0
+        return int(lines)
 
 
 def log_size(connection, filename) -> int:
-    lines = ' '.join(send_command(
+    lines = ' '.join(get_verified_response_for_command(
         connection,
         'log_size',
         filename,
@@ -72,12 +163,26 @@ def log_size(connection, filename) -> int:
     return int(lines)
 
 
+def parse_date(date_str):
+    return datetime.datetime.strptime(
+        date_str,
+        "%Y-%m-%d"
+    )
+
+
+def get_local_file_checksum(filename) -> int:
+    with open(filename, 'rb') as inf:
+        data = inf.read()
+
+    return binascii.crc32(data)
+
+
 def main(*args):
     parser = argparse.ArgumentParser()
     parser.add_argument('port')
     parser.add_argument('destination')
     parser.add_argument('--log-level', default='INFO')
-    parser.add_argument('--date')
+    parser.add_argument('--date', type=parse_date)
     args = parser.parse_args()
 
     logging.basicConfig(
@@ -89,18 +194,37 @@ def main(*args):
     logs = list(list_logs(connection))
     for filename in logs:
         logger.info('Filename: %s', filename)
-        remote_size = log_size(connection, filename)
-        local_size = 0
+
+        try:
+            filename_date = datetime.datetime.strptime(
+                filename,
+                "%Y/%m/%d/%H%M%S.log"
+            )
+        except ValueError:
+            logger.warning("Unable to parse date.")
+            filename_date = datetime.datetime(1970, 1, 1)
+
+        if (
+            args.date and
+            (
+                filename_date < args.date or
+                filename_date > args.date + datetime.timedelta(days=1)
+            )
+        ):
+            logger.info("Unwanted date")
+            continue
+
+        checksum = log_checksum(connection, filename)
+        local_checksum = 0
         local_path = os.path.join(
             args.destination,
             filename
         )
 
         if os.path.exists(local_path):
-            local_size = os.stat(local_path).st_size
+            local_checksum = get_local_file_checksum(local_path)
 
-        logger.debug('Local %sb ?= Remote %sb', local_size, remote_size)
-        if local_size >= remote_size:
+        if local_checksum == checksum:
             logger.info('Already downloaded')
             continue
 
